1432ff45aa51cea8f98e1135502d3be86f4cc8c0
Xu Jing <xuj@myscale.com>
2024-05-27 18:43:40 +0800

    reformat

--- myscale_telemetry/handler.py
+++ myscale_telemetry/handler.py
@@ -115,11 +115,13 @@ def _extract_span_attributes(data: Any, **kwargs: Any) -> Dict[str, str]:
 
     return span_attributes
 
+
 def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
 
 class MyScaleCallbackHandler(BaseCallbackHandler):
     """Callback Handler for MyScale.
